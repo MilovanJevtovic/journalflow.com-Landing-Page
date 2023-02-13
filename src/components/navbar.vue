9f6bfb9c7efae4429ab98611c5c784f75748b9d6
MilovanJevtovic <72308653+MilovanJevtovic@users.noreply.github.com>
2023-02-13 12:08:29 +0100

    Finished phone view

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,32 +1,56 @@
 <template>
     <main>
+      <div v-if="showOverlay" class="overlay">
+        <div class="modal">
+          <button>Home</button>
+          <button>Blog</button>
+          <button>Pricing</button>
+          <button>Support</button>
+          <button>Login</button>
+          <div>
+            <button @click="showOverlay = false" class="close"><img src="./icons/delete-symbol-1-white.svg" alt=""></button>
+          </div>
+        </div>
+      </div>
       <div class="wrap">
         <div class="icon-name">
           <img src="./icons/logo-white.png">
           <h1>JournalFlow</h1>
         </div>
-        <div>
+        <div class="buttons">
           <button>Features</button>
           <button>Blog</button>
           <button>Pricing</button>
           <button>Support</button>
           <button>Login</button>
         </div>
+        <button @click="showOverlay = true" class="hamburger"><img src="./icons/menu.svg"></button>
       </div>
     </main>
   </template>
-  
+
   <script>
 
-  export default{
-    name:'Navbar',
+import {ref} from "vue"
+
+const showOverlay = ref(false)
+
+export default{
+  name:'Navbar',
+  data(){
+    return{
+      showOverlay: false,
+    }
   }
+}
 
-
-  
-  </script>
+</script>
   
   <style scoped>
+
+  .hamburger{
+    display: none;
+  }
 
   main{
     background-color: #0B182B;
@@ -55,7 +79,7 @@
     font-weight: 500;
   }
   
-  button{
+   .buttons button{
     background-color: transparent;
     border: none;
     color: #9EA0A6;
@@ -72,16 +96,73 @@
   }
   
   
-  img{
+  .icon-name img{
     width: 32px;
     height: 32px;
     margin-right: 6px;
   }
 
   @media (max-width: 700px) {
-    button{
+    .buttons button{
       display: none;
     }
+
+    .close{
+      position: absolute;
+      top: 15px;
+      right: -175px;
+    }
+
+    .close img{
+      width: 50px;
+      height: 50px;
+    }
+
+    .hamburger{
+      display: block;
+      background: transparent;
+      padding: 0px 0px;
+      border: none;
+      margin-right: 0px;
+    }
+
+    .hamburger img{
+      height: 40px;
+      width: 40px;
+    }
+
+    .overlay{
+      position: absolute;
+      z-index: 10;
+      width: 100%;
+      height: 100%;
+      background-color: #0b182b;
+      opacity: 0.95;
+      padding-top: 80px;
+      display: flex;
+      justify-content: center;
+    }
+  
+    .modal{
+      display: flex;
+      flex-direction: column;
+    }
+
+    .modal button{
+      width: 100%;
+      text-align: center;
+      font-size: 24px;
+      line-height: 60px;
+      font-weight: 500;
+      background-color: transparent;
+      border: none;
+      color: white;
+    }
+  
+    .modal button:hover{
+      text-decoration: underline;
+    }
+
   }
   
   
